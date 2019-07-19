5c1060633ebc35eb9649bc4a2209b2bd04ce1e86
Courtnee M <courtneemeaghan@gmail.com>
2019-07-19 02:05:54 -0500

    added reset button and custom grid size

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const parentDiv = document.querySelector('#parent-div');
+const resetBtn = document.querySelector('#reset-btn');
+let userGrid;
 
 function createDiv(gridSize) {
+  if(isNaN(gridSize)) {
+    while(isNaN(gridSize)) {
+      gridSize = Number(prompt("Please choose a number for the grid size. Between 1 and 50 is recommended."));
+    };
+  }
+
   let gridRow = (800 / gridSize);
   let gridBox = gridRow - 2;
   for(let i = 0; i < gridSize * gridSize; i++) {
     let newDiv = document.createElement('div');
+    newDiv.classList.add('grid');
     newDiv.style.width = `${gridBox}px`;
     parentDiv.appendChild(newDiv);
   }
 
-  let square = parentDiv.querySelectorAll('div');
+  fillGrid();
+}
 
+function fillGrid() {
   document.addEventListener('mouseover', function(e) {
-    if(e.target.tagName === "DIV" && e.target.id != "#parent-div") {
-      e.target.classList.toggle('square');
+    if(e.target.className === "grid") {
+      e.target.classList.add('square');
     }
   });
 }
 
-createDiv(16);
+resetBtn.addEventListener('click', function() {
+  let numChildren = parentDiv.children.length;
+  for(let i = 0; i < numChildren; i++) {
+    parentDiv.removeChild(parentDiv.firstChild);
+  }
+  
+  userGrid = Number(prompt("Pick a new grid size. Between 1 and 50 recommended."));
+  createDiv(userGrid);
+});
 
+
+
+
+createDiv(16);
